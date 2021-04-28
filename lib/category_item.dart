203cb6674b0b58e34777af1511eac95bec654fb9
Dunatron <heath.dunlop@op.ac.nz>
2021-04-28 17:11:29 +1200

    ff

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -9,9 +9,13 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(MaterialPageRoute(builder: (_) {
-      return CategoryMealsScreen(id, title);
-    }));
+    // Navigator.of(ctx).push(
+    //   MaterialPageRoute(builder: (_) {
+    //     return CategoryMealsScreen(id, title);
+    //   }),
+    // );
+    Navigator.of(ctx)
+        .pushNamed('/categories', arguments: {'id': id, 'title': title});
   }
 
   @override
