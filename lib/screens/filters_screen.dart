026f1d87c0b23f4de5fe55f582bd915d6928114e
Dunatron <heath.dunlop@op.ac.nz>
2021-05-03 11:26:43 +1200

    ff

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+
+class FilterScreen extends StatefulWidget {
+  static const routeName = '/filters';
+
+  @override
+  _FilterScreenState createState() => _FilterScreenState();
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  bool _glutenFree = false;
+  bool _vegetarian = false;
+  bool _vegan = false;
+  bool _lactoseFree = false;
+
+  Widget _buildSwitchListTile(String title, String description,
+      bool currentValue, Function updateValue) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Your Filters')),
+      drawer: MainDrawer(),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust Meal Selection',
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _buildSwitchListTile('Gluten-free',
+                    'Only include gluten -ree meals', _glutenFree, (newValue) {
+                  setState(() {
+                    _glutenFree = newValue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Lactose-free',
+                    'Only include lactose-free meals',
+                    _lactoseFree, (newValue) {
+                  setState(() {
+                    _lactoseFree = newValue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Vegetarian', 'Only include vegetarian meals', _vegetarian,
+                    (newValue) {
+                  setState(() {
+                    _vegetarian = newValue;
+                  });
+                }),
+                _buildSwitchListTile(
+                    'Vegan', 'Only include vegan meals', _vegan, (newValue) {
+                  setState(() {
+                    _vegan = newValue;
+                  });
+                })
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/widgets/main_drawer.dart
+++ lib/widgets/main_drawer.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MainDrawer extends StatelessWidget {
-  Widget buildListTile(String title, IconData icon) {
+  Widget buildListTile(String title, IconData icon, Function tapHandler) {
     return ListTile(
       leading: Icon(
         // Icons.restaurant,
@@ -15,6 +15,7 @@ class MainDrawer extends StatelessWidget {
           fontSize: 24,
         ),
       ),
+      onTap: tapHandler,
     );
   }
 
@@ -37,21 +38,12 @@ class MainDrawer extends StatelessWidget {
           ),
         ),
         SizedBox(height: 20),
-        buildListTile('Meals', Icons.restaurant),
-        buildListTile('Filters', Icons.settings)
-        // ListTile(
-        //   leading: Icon(
-        //     Icons.restaurant,
-        //     size: 26,
-        //   ),
-        //   title: Text(
-        //     'Meals',
-        //     style: TextStyle(
-        //       fontFamily: 'RobotoCondensed',
-        //       fontSize: 24,
-        //     ),
-        //   ),
-        // )
+        buildListTile('Meals', Icons.restaurant, () {
+          Navigator.of(context).pushReplacementNamed('/');
+        }),
+        buildListTile('Filters', Icons.settings, () {
+          Navigator.of(context).pushReplacementNamed('/filters');
+        })
       ]),
     );
   }
